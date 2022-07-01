17fa8e45e739c7d23e6b8261e207eddc9de58be9
Timur <gimboy09@mail.ru>
2022-07-01 23:47:42 +0500

    Start

---
+++ src/main/java/com/gimMusic/gimMusic/GimMusicApplication.java
@@ -0,0 +1,13 @@
+package com.gimMusic.gimMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimMusic/gimMusic/GimMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimMusic.gimMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
